48b737686f4bf6609252c3cdaca4b16562c76d69
Nandhu9345 <144416350+Nandhu9345@users.noreply.github.com>
2023-09-11 19:22:22 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,9 @@
+# Factorial number 
+def fact(n):
+  if n==0 or n==1:
+    return 1
+  else:
+   return n*fact(n-1)
+number = int(input("enter  the value="))
+result =fact(number)
+print("the factorial of{} is {}.".format(number, result))
